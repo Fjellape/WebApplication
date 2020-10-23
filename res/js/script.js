a8ab3499b2a2340c383a81359b420b3cc4a4384b
Kristo V <smileshow2013@gmail.com>
2020-10-24 02:36:28 +0300

    Task #2 Fetched endpoint except media

--- res/js/script.js
+++ res/js/script.js
@@ -56,4 +56,26 @@ $(document).ready(function() {
         }
     })
 
+    //index page for post
+    $.get('https://private-anon-a1e1b8d498-wad20postit.apiary-mock.com/posts', function (response) {
+        for (post of response) {
+            let div = $('<div class="post">');
+
+            let userName = $('<h1>').text(post.author.firstname + " " + post.author.lastname);
+            let img = $('<img>').attr('src', post.author.avatar);
+            let time = $('<p>').text(post.createTime);
+            let text = $('<p>').text(post.text);
+            let likes = $('<p>').text(post.likes);
+            // Need to add media. Media can be empty. Media can be images or video.
+
+            div.append(img)
+            div.append(userName)
+            div.append(time)
+            div.append(text)
+            div.append(likes)
+
+            $('#post').append(div)
+        }
+    })
+
 });
